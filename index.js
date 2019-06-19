ce0a9f3ff902453f934502520f6e3786ef3a12cc
admin <rtwallace23@gmail.com>
2019-06-19 17:43:44 -0400

    Array added to demonstrate recurrence loggin in console.

---
+++ index.js
@@ -0,0 +1,15 @@
+// count to 5 both backwards and forwards
+
+const trainingData = [
+    [1,2,3,4,5,],
+    [5,4,3,2,1,]
+];
+
+const net =  new brain.recurrent.LSTMTimeStep();
+
+net.train(trainingData);
+
+console.log(net.run([1,2,3,4]));
+console.log(net.run([5,4,3,2]));
+
+
